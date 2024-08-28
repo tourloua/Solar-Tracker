7ea996a0ac926a3b89018d7bc2048c40ad9d7809
tourloua <144152554+tourloua@users.noreply.github.com>
2024-08-28 15:14:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+ 
+  
+  const backButton = document.getElementById('backButton');
+
+  if (backButton) {
+    backButton.addEventListener('click', function () {
+      window.location.href = 'https://tourloua.github.io/alextourloukis/';//change link to project main page
+    });
+  }
+
+  
+})
